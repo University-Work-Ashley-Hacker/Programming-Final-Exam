55dc2c63ae11a412e0e2bdc3c1768053e90b755f
Ashley Hacker <ashleyhacker0127@gmail.com>
2023-11-30 13:37:13 -0500

    I litterally added an animation what is wrong with me

--- Assets/_Scripts/UIManager.cs
+++ Assets/_Scripts/UIManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,30 +9,42 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject _gameOverUI;
     [SerializeField] private TextMeshProUGUI _scoreText;
     [SerializeField] private TextMeshProUGUI _highScoreText;
+    [SerializeField] private TextMeshProUGUI _UIScoreText;
     private void _startGame()
     {
         _startText.SetActive(false);
+        _UIScoreText.transform.parent.gameObject.SetActive(true);
+        _UIScoreText.transform.parent.transform.DOMoveX(100, 1);
     }
 
     private void OnEnable()
     {
         GameManager.StartGame += _startGame;
+        GameManager.GemGained += ShowScore;
         GameManager.LoseGame += _loseGame;
     }
     private void OnDisable()
     {
         GameManager.StartGame -= _startGame;
+        GameManager.GemGained -= ShowScore;
         GameManager.LoseGame -= _loseGame;
     }
     private void _loseGame()
     {
         _gameOverUI.SetActive(true);
+        _UIScoreText.transform.parent.transform.DOMoveX(-300, 1);
         _scoreText.text = "Score: " + GameManager.Gems;
         _highScoreText.text = "High Score: " + ScoreManager.HighScore;
     }
 
+    private void ShowScore()
+    {
+        _UIScoreText.text = GameManager.Gems.ToString();
+    }
+
     public void PlayAgainButton()
     {
+        _UIScoreText.transform.parent.transform.DOKill();
         SceneManager.LoadScene("ZigZag");
     }
     public void QuitButton()
